50c8d1a46327d497d4bfad54bc535b9dbb127f08
Shinichiro Suzuki <shintrfc@gmail.com>
2021-03-13 19:53:01 +0900

    implimenting userEditComponent

--- screens/UserEditScreen.tsx
+++ screens/UserEditScreen.tsx
@@ -5,9 +5,15 @@ import { Button, StyleSheet, Text, View } from "react-native";
 
 import { BottomTabParamList, RootStackParamList } from "../types";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import Colors from "../constants/Colors";
 
 type UserEditScreenProps = {
 	navigation: StackNavigationProp<RootStackParamList, "UserEdit">;
+};
+
+type SettingButtonButtonProps = {
+	text: string;
+	additional?: string;
 };
 
 const UserEditScreen: React.FC<UserEditScreenProps> = (props) => {
@@ -16,26 +22,76 @@ const UserEditScreen: React.FC<UserEditScreenProps> = (props) => {
 	const onPress = () => {
 		navigation.goBack();
 	};
-	return (
-		<View>
-			<TouchableOpacity>
-				<Text>登録済みEmailの変更</Text>
+	const SettingButton: React.FC<SettingButtonButtonProps> = (props) => {
+		return (
+			<TouchableOpacity style={styles.buttonBox}>
+				<View style={{ flexDirection: "row", alignItems: "center" }}>
+					<AntDesign name='leftsquare' size={20} />
+					<View
+						style={{
+							alignItems: "flex-start",
+							flexDirection: "column",
+						}}
+					>
+						<Text style={styles.ButtonText}>{props.text}</Text>
+						{props.additional && (
+							<Text style={styles.ButtonText}>
+								{props.additional}
+							</Text>
+						)}
+					</View>
+				</View>
 				<AntDesign name='arrowright' size={25} />
 			</TouchableOpacity>
-			<TouchableOpacity>
-				<Text>パスワードの変更</Text>
-				<AntDesign name='arrowright' size={25} />
-			</TouchableOpacity>
-			<TouchableOpacity>
-				<Text>ユーザーネームを変更</Text>
-				<AntDesign name='arrowright' size={25} />
-			</TouchableOpacity>
+		);
+	};
 
-			<Button title='戻る' onPress={onPress} />
-		</View>
+	return (
+		<>
+			<View style={styles.container}>
+				<Text style={styles.text}>基本設定</Text>
+				<View>
+					{/* <Text style={styles.text}>基本設定</Text> */}
+					<SettingButton
+						text='emailの変更'
+						additional='email@gmail.com'
+					/>
+					<SettingButton
+						text='ユーザーネームを変更'
+						additional='agub1994'
+					/>
+					<SettingButton text='パスワードの変更' />
+					<Button title='戻る' onPress={onPress} />
+				</View>
+			</View>
+		</>
 	);
 };
 
 export default UserEditScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		// padding: 20,
+		// backgroundColor: "white",
+	},
+	ButtonText: {
+		// paddingHorizontal: 20,
+	},
+	text: {
+		padding: 20,
+		width: "100%",
+		fontWeight: "bold",
+		backgroundColor: Colors.light.background,
+		color: Colors.light.textLight,
+	},
+	buttonBox: {
+		paddingVertical: 5,
+		paddingHorizontal: 20,
+		backgroundColor: "white",
+		alignItems: "center",
+		flexDirection: "row",
+		justifyContent: "space-between",
+	},
+});
