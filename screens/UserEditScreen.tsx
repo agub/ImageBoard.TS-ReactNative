95f8fd4ed933375834c7caa85d931b245388a061
Shinichiro Suzuki <shintrfc@gmail.com>
2021-04-04 18:24:43 +0900

    implementing change password function

--- screens/UserEditScreen.tsx
+++ screens/UserEditScreen.tsx
@@ -1,11 +1,12 @@
 import { StackNavigationProp } from "@react-navigation/stack";
 import { AntDesign, Feather } from "@expo/vector-icons";
-import React from "react";
+import React, { useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 
 import { BottomTabParamList, RootStackParamList } from "../types";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
 import Colors from "../constants/Colors";
+import { Auth } from "aws-amplify";
 
 type UserEditScreenProps = {
 	navigation: StackNavigationProp<RootStackParamList, "UserEdit">;
@@ -22,9 +23,29 @@ const UserEditScreen: React.FC<UserEditScreenProps> = (props) => {
 	const onPress = () => {
 		navigation.goBack();
 	};
+
+	const changePasswordHandler = (
+		oldPassword: string,
+		newPassword: string
+	) => {
+		Auth.currentAuthenticatedUser()
+			.then((user) => {
+				return Auth.changePassword(user, oldPassword, newPassword);
+			})
+			.then((data) => console.log(data))
+			.catch((err) => console.log(err));
+	};
+
+	const [changePassword, sethangePassword] = useState(false);
+	const [currentPassword, setCurrentPassword] = useState("");
+	const [newPassword, setNewPassword] = useState("");
+
 	const SettingButton: React.FC<SettingButtonButtonProps> = (props) => {
 		return (
-			<TouchableOpacity style={styles.buttonBox}>
+			<TouchableOpacity
+				onPress={() => sethangePassword(!changePassword)}
+				style={styles.buttonBox}
+			>
 				<View
 					style={{
 						flexDirection: "row",
@@ -71,11 +92,59 @@ const UserEditScreen: React.FC<UserEditScreenProps> = (props) => {
 						text='emailの変更'
 						additional='email@gmail.com'
 					/>
+
 					<SettingButton
 						text='ユーザーネームを変更'
 						additional='agub1994'
 					/>
 					<SettingButton text='パスワードの変更' />
+					{changePassword && (
+						<View
+							style={{
+								flexDirection: "row",
+								justifyContent: "space-between",
+								alignItems: "center",
+								backgroundColor: "white",
+								paddingHorizontal: 25,
+							}}
+						>
+							<View>
+								<TextInput
+									blurOnSubmit={false}
+									autoFocus={true}
+									placeholder='現在のパスワード'
+									// keyboardType='visible-password'
+									secureTextEntry={true}
+									placeholderTextColor={
+										Colors.light.textLight
+									}
+									style={{ paddingVertical: 10 }}
+									value={currentPassword}
+									onChangeText={setCurrentPassword}
+								/>
+								<TextInput
+									blurOnSubmit={false}
+									placeholder='新しいパスワード'
+									placeholderTextColor={
+										Colors.light.textLight
+									}
+									style={{
+										paddingVertical: 20,
+									}}
+								/>
+							</View>
+							<View
+								style={
+									{
+										// paddingVertical: 20,
+									}
+								}
+							>
+								<Text>変更する</Text>
+							</View>
+						</View>
+					)}
+
 					<View style={styles.backBtn}>
 						<Button title='戻る' onPress={onPress} />
 					</View>
