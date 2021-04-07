124a501cd99d638251dc77f939285afbd24a8bfa
Shinichiro Suzuki <shintrfc@gmail.com>
2021-04-08 01:46:51 +0900

    app.js rooter file has changed

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
-import { Navigation } from "./navigation/Index";
+import { Navigation } from "./navigation/index";
 import Amplify, { API, Auth, graphqlOperation } from "aws-amplify";
 // @ts-ignore
 import { withAuthenticator } from "aws-amplify-react-native";
